8411000814365b18b2069facf729e367e428ab25
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2024-12-31 21:31:23 +0900

    ✨ スクリーンショット関連のコマンドをいれる

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,80 @@
+use std::env;
+
+use base64::{engine::general_purpose, Engine};
+use serde::{Deserialize, Serialize};
+
+mod screenshot;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn get_api_key() -> Result<String, String> {
+    dotenv::from_filename(".env.local").ok();
+    env::var("GENAI_API_KEY").map_err(|_| "GENAI_API_KEY is not set".to_string())
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct ScreenShot {
+    /** base64 encoded */
+    pub image: String,
+    pub xy: (i32, i32),
+    pub wh: (u32, u32),
+}
+#[tauri::command]
+async fn take_screen_shot() -> Result<ScreenShot, String> {
+    let image_data = screenshot::take_screen_shot().map_err(|e| e.to_string())?;
+    let mut buf = Vec::new();
+    let mut cursor = std::io::Cursor::new(&mut buf);
+    image_data
+        .image
+        .write_to(&mut cursor, image::ImageFormat::Png)
+        .map_err(|e| e.to_string())?;
+    let base64 = general_purpose::STANDARD.encode(&buf);
+    Ok(ScreenShot {
+        image: base64,
+        xy: image_data.xy,
+        wh: image_data.wh,
+    })
+}
+
+#[tauri::command]
+async fn crop_image(image: String, xy: (i32, i32), wh: (u32, u32)) -> Result<String, String> {
+    if xy.0 < 0 || xy.1 < 0 {
+        return Err("xy must be positive".to_string());
+    }
+    if wh.0 == 0 || wh.1 == 0 {
+        return Err("wh must be positive".to_string());
+    }
+
+    let buf = general_purpose::STANDARD
+        .decode(image.as_bytes())
+        .map_err(|e| e.to_string())?;
+    let mut image = image::load_from_memory(&buf).map_err(|e| e.to_string())?;
+    let cropped_image = image.crop(xy.0 as u32, xy.1 as u32, wh.0, wh.1);
+    let mut buf = Vec::new();
+    let mut cursor = std::io::Cursor::new(&mut buf);
+    cropped_image
+        .write_to(&mut cursor, image::ImageFormat::Png)
+        .map_err(|e| e.to_string())?;
+    let base64 = general_purpose::STANDARD.encode(&buf);
+    Ok(base64)
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            get_api_key,
+            take_screen_shot,
+            crop_image
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/screenshot.rs
@@ -0,0 +1,80 @@
+use image::RgbaImage;
+
+struct MonitorInfo {
+    xy: (i32, i32),
+    wh: (u32, u32),
+    image: RgbaImage,
+}
+struct VirtualImageSize {
+    xy: (i32, i32),
+    wh: (u32, u32),
+}
+pub struct VirtualImage {
+    pub xy: (i32, i32),
+    pub wh: (u32, u32),
+    pub image: RgbaImage,
+}
+
+pub fn take_screen_shot() -> anyhow::Result<VirtualImage> {
+    let monitors = xcap::Monitor::all()?;
+    let monitor_infos = monitors
+        .into_iter()
+        .map(|monitor| {
+            let image = monitor.capture_image()?;
+            Ok(MonitorInfo {
+                xy: (monitor.x(), monitor.y()),
+                wh: (monitor.width(), monitor.height()),
+                image,
+            })
+        })
+        .collect::<anyhow::Result<Vec<_>>>()?;
+    let merged_image = merge_images(&monitor_infos)?;
+
+    Ok(merged_image)
+}
+
+/**
+ * Calculate the size that encompasses all the monitors.
+ */
+fn get_whole_size(monitor_infos: &[MonitorInfo]) -> anyhow::Result<VirtualImageSize> {
+    let x = monitor_infos
+        .iter()
+        .map(|info| info.xy.0)
+        .min()
+        .ok_or(anyhow::anyhow!("error in calculating x"))?;
+    let y = monitor_infos
+        .iter()
+        .map(|info| info.xy.1)
+        .min()
+        .ok_or(anyhow::anyhow!("error in calculating y"))?;
+    let w = monitor_infos
+        .iter()
+        .map(|info| info.xy.0 + info.wh.0 as i32)
+        .max()
+        .ok_or(anyhow::anyhow!("error in calculating w"))?
+        - x;
+    let h = monitor_infos
+        .iter()
+        .map(|info| info.xy.1 + info.wh.1 as i32)
+        .max()
+        .ok_or(anyhow::anyhow!("error in calculating h"))?
+        - y;
+
+    Ok(VirtualImageSize {
+        xy: (x, y),
+        wh: (w as u32, h as u32),
+    })
+}
+fn merge_images(monitor_infos: &[MonitorInfo]) -> anyhow::Result<VirtualImage> {
+    let VirtualImageSize { xy, wh } = get_whole_size(monitor_infos)?;
+    let mut image = RgbaImage::new(wh.0, wh.1);
+    for info in monitor_infos {
+        image::imageops::overlay(
+            &mut image,
+            &info.image,
+            (info.xy.0 - xy.0) as i64,
+            (info.xy.1 - xy.1) as i64,
+        );
+    }
+    Ok(VirtualImage { xy, wh, image })
+}
