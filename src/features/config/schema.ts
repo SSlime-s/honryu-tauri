c6e13e5f47a6c30ef4c36febc24a07ae91922a1f
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2025-02-15 00:20:25 +0900

    ⚙️ 使えるモデルを更新

--- src/features/config/schema.ts
+++ src/features/config/schema.ts
@@ -4,7 +4,11 @@ export const models = {
 	"gemini-1.5-flash": "Gemini 1.5 Flash",
 	"gemini-1.5-8b": "Gemini 1.5 8B",
 	"gemini-1.5-pro": "Gemini 1.5 Pro",
-	"gemini-2.0-flash-exp": "Gemini 2.0 Flash Experimental",
+	"gemini-2.0-flash": "Gemini 2.0 Flash",
+	"gemini-2.0-flash-lite-preview-02-05": "Gemini 2.0 Flash-Lite Preview 02-05",
+	"gemini-2.0-pro-exp-02-05": "Gemini 2.0 Pro Experimental 02-05",
+	"gemini-2.0-flash-thinking-exp-01-21":
+		"Gemini 2.0 Flash Thinking Experimental 01-21",
 } as const;
 export const modelKeys = Object.keys(models) as (keyof typeof models)[];
 
